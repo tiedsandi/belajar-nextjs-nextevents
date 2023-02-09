e153dd349ddc7d4c779645e3afefd1b009301c29
tiedSandi <wkwkwkand@gmail.com>
2023-02-09 09:33:41 +0700

    get data and show events featured in home page

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,13 @@
+import EventList from "../components/events/event-list";
+import { getFeaturedEvents } from "../dummy-data";
+
 const HomePage = () => {
+    const featuredEvents = getFeaturedEvents();
+
     return (
+        //  Show Featured Events
         <div>
-            {/* Show Featured Events */}
-            <h1>Home Page</h1>
+            <EventList items={featuredEvents} />
         </div>
     );
 };
