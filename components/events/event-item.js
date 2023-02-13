d632908abcf5bd26268b3dcb276fe3ffcbd9b82b
tiedSandi <wkwkwkand@gmail.com>
2023-02-13 07:04:58 +0700

    add event item

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,5 +1,7 @@
 import Link from "next/link"
 
+import classes from './event-item.module.css';
+
 const EventItem = (props) => {
     const { title, image, date, location, id } = props;
 
@@ -12,23 +14,23 @@ const EventItem = (props) => {
     const explorLink = `/events/${id}`;
 
     return (
-        <li>
+        <li className={classes.item}>
             <img src={"/" + image} alt={title} />
-            <div>
-                <div>
+            <div className={classes.content}>
+                <div className={classes.summary}>
                     <h2>{title}</h2>
-                    <div>
+                    <div className={classes.date} >
                         <time>{humanReadableDate}</time>
                     </div>
-                    <div>
+                    <div className={classes.address}>
                         <address>{formattedAddress}</address>
                     </div>
                 </div>
-                <div>
+                <div className={classes.action}>
                     <Link href={explorLink}>Explore Event</Link>
                 </div>
             </div>
-        </li>
+        </li >
     )
 }
 
--- components/events/event-list.js
+++ components/events/event-list.js
@@ -1,10 +1,12 @@
 import EventItem from "./event-item";
 
+import clases from './event-list.module.css'
+
 const EventList = (props) => {
     const { items } = props;
 
     return (
-        <ul>
+        <ul className={clases.list}>
             {items.map(event => (
                 <EventItem
                     key={event.id}
